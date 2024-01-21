640965707b6852445683d48e99c6987be259119a
Rodrigo Amaral <rodrigoamaral1705@gmail.com>
2024-01-21 20:55:55 -0300

    ultimo commit

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,13 @@
 import React, { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/auth";
+import { FcMenu } from "react-icons/fc";
 
 import CartButton from "../Cartbutton/CartButton";
 import AppContext from "../../contexts/AppContext";
-import "./Navbar.css";
 import Cart from "../Cart/Cart";
 import NavbarMenu from "./NavbarMenu";
-
-import { FcMenu } from "react-icons/fc";
+import "./Navbar.css";
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -25,52 +24,69 @@ const Navbar = () => {
 
   const toggleMenuActive = () => {
     setMenuActive(!menuActive);
-  }
+  };
+
+  const boo = () => {
+    alert("BOO!!");
+  };
 
   return (
     <div>
-
-    <header>
-      <a href="/">
-        <img src="/src/img/Icone.png" alt="Icone" width="50" height="45" />
-      </a>
-      <h1>Afood</h1>
-      <h2>
-        Olá, <span onClick={()=>{navigate('/user')}} >{user.name}!</span>
-      </h2>
-      <ul>
-        <CartButton className='icone_carrinho'/>
-        <li>
-          <button
-          className="logout-btn"
-          type="button"
-          onClick={() => {
-            navigate("/");
-          }}>Cardápio</button>
-        </li>
-        <li>
-          {user.scope ==="adm" &&
-            <button
-            className="logout-btn"
-            type="button"
+      <header>
+        <a onClick={() => boo()}>
+          <img src="/src/img/Icone.png" alt="Icone" width="50" height="45" />
+        </a>
+        <h1>Afood</h1>
+        <h2>
+          Olá,{" "}
+          <span
             onClick={() => {
-              navigate("/pedidos");
+              navigate("/user");
             }}
           >
-            Pedidos
-          </button>
-          }
-        </li>
-        <li>
-          <button className="logout-btn" type="button" onClick={handleLogout}>
-            Logout
-          </button>
-        </li>
-      </ul>
-      <FcMenu onClick={toggleMenuActive} className="icone_hamburguer"/>
-      {menuActive ? <NavbarMenu user={user} onLogout={handleLogout} /> : <></>}
-    </header>
-    <Cart />
+            {user.name}!
+          </span>
+        </h2>
+        <ul>
+          <CartButton className="icone_carrinho" />
+          <li>
+            <button
+              className="logout-btn"
+              type="button"
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              Cardápio
+            </button>
+          </li>
+          <li>
+            {user.scope === "adm" && (
+              <button
+                className="logout-btn"
+                type="button"
+                onClick={() => {
+                  navigate("/pedidos");
+                }}
+              >
+                Pedidos
+              </button>
+            )}
+          </li>
+          <li>
+            <button className="logout-btn" type="button" onClick={handleLogout}>
+              Logout
+            </button>
+          </li>
+        </ul>
+        <FcMenu onClick={toggleMenuActive} className="icone_hamburguer" />
+        {menuActive ? (
+          <NavbarMenu user={user} onLogout={handleLogout} />
+        ) : (
+          <></>
+        )}
+      </header>
+      <Cart />
     </div>
   );
 };
